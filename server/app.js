877a818edcb7ef425f3766e11422e72a001fc3bf
ahmedfayazyousuf <ahmedfayazyousuf@gmail.com>
2022-10-23 20:33:56 +0400

    dd
    dd

--- server/app.js
+++ server/app.js
@@ -35,46 +35,6 @@ app.get('/signup', (req, res) => {
  
 
 
-app.post("/timetaken",(req,res) =>{
-
-    Admin.find({}, function(err, admin) {
-        console.log(admin);
-
-        time = admin[0].Time;
-        const NameData = new Movie({User:req.body.Name,Score:0});
-    console.log(req.body);
-    NameData.save().then((movie) => {
-        // console.log(movie.id);
-        // name = movie.Name;
-    }).catch(err => {
-        res.status(400).send("unable to save to database");
-      });
-      name = NameData._id;
-      user = NameData.User;
-      res.render("game",{time:time});
-    });
-
-    
-      
-})
-
-
-
-
-app.get("/l",(req,res) =>{
-    // console.log(name)
-    User.find({"timetaken":{$ne : 0}}, function(err, users) {
-        // console.log(name);
-        // console.log(score);
-        res.render('leaderboard', {
-            usersList: users,
-            time: timetaken,
-            usera:user
-        })
-    }).sort({Score : -1}).limit(5);
-})
-
-
 
 app.listen(PORT, () => {    
     console.log(`The Server is running on port ${PORT}`);
